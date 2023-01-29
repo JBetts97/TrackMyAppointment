cb87ee253813ebbcec456add7f4223d89429f92d
Joseph Betts <joe.betts97@gmail.com>
2023-01-29 20:26:04 +0000

    Updated gitignore

---
+++ src/main/java/jbetts/trackmyappointment/TrackmyappointmentApplication.java
@@ -0,0 +1,13 @@
+package jbetts.trackmyappointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackmyappointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackmyappointmentApplication.class, args);
+	}
+
+}
